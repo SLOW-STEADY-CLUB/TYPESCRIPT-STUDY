be53ac2e7b422d00c7470e4be148e4105d37d85f
윤선용 <sunyong9059@naver.com>
2022-11-19 15:32:55 +0900

    atom components 생성

---
+++ TODO/sunyong/src/recoil/atoms.ts
@@ -0,0 +1,16 @@
+import { atom } from "recoil";
+
+  export const itemList = atom({
+    key: 'itemList',
+    default: []
+  });
+
+
+  export const postItem = atom({
+    key: 'postItem',
+    default: [{
+      title: "오늘의 투두들",
+      isDone:true,
+      id: 0
+    }]
+  });
