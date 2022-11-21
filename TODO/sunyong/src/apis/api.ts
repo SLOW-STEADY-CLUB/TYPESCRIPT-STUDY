a1276d7fb7dc6950d8871799ccab17c8afe2a9d8
윤선용 <sunyong9059@naver.com>
2022-11-22 04:30:52 +0900

    에러 핸들링 처리

--- TODO/sunyong/src/apis/api.ts
+++ TODO/sunyong/src/apis/api.ts
@@ -1,31 +1,35 @@
 import axios from "axios";
+import { Todo } from "components/TypeList";
 
 export const itemGet = async () => {
     try{
         const res = await axios.get('http://localhost:3001/comments')
-        console.log(res)
         return res.data
     }catch(err) {
         console.log(err)
-    }
+    }   
 }
 
 export const itemPost = async (data : any) => {
+    const answer = true
     try{
-        console.log(data)
+        console.log(data.length)
+        if(data.length === 1) {
+            alert('TODO List는 꼭 입력해주세요')
+            return
+        }
         const res = await axios.post('http://localhost:3001/comments', data)
-        
-        console.log(res)
-        return res.data
+        return answer
     }catch(err) {
         console.log(err)
+        return !answer
     }
 }
 
 export const DeletePost = async (data : any) => {
     try{
         console.log(data)
-        const res = await axios.delete(`http://localhost:3001/items/${data}`)
+        const res = await axios.delete(`http://localhost:3001/comments/${data}`)
         console.log(res)
         return res.data
     }catch(err) {
